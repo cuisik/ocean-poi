bef487a38726b6a1d8f262cd0b39ced02a048e36
miaof <704420748@qq.com>
2018-04-24 01:16:44 +0800

    2018 4 24

--- src/main/java/io/sssd/model/Field.java
+++ src/main/java/io/sssd/model/Field.java
@@ -1,5 +1,7 @@
 package io.sssd.model;
 
+import java.util.Arrays;
+
 /**
  * Created by MIAOM on 2018/4/22.
  */
@@ -9,9 +11,19 @@ public class Field {
 
     private String likeName;
 
+    protected String getFieldName() {
+        return fieldName;
+    }
+
+    protected String getLikeName() {
+        return likeName;
+    }
+
     private boolean isProtect;
 
     private String formula;
+
+    private boolean isLeaf = true;
 
     public Field(String fieldName, String likeName) {
         this.fieldName = fieldName;
@@ -47,6 +59,106 @@ public class Field {
 
     private Field[] childFiles;
 
+    protected Field[] getChildFiles() {
+        return childFiles;
+    }
+
+
+
+    public Field(String fieldName, String likeName, Field[] childFiles) {
+        this.fieldName = fieldName;
+        this.likeName = likeName;
+        this.childFiles = childFiles;
+        this.breath = reBreath(childFiles);
+        this.depth = reDepth(childFiles);
+        this.isLeaf = false;
+    }
+
+    public Field(String fieldName, Field[] childFiles) {
+        this.fieldName = fieldName;
+        this.likeName = fieldName;
+        this.childFiles = childFiles;
+        this.breath = reBreath(childFiles);
+        this.depth = reDepth(childFiles);
+        this.isLeaf = false;
+    }
+
+    // 运行时 相关属性
+    // 节点的广度 : 有多少个子节点 逐步返回上级
+    private int breath;
+    // 节点的深度 : 有多少层子节点 逐步返回上级
+    private int depth;
+
+    // 合并单元格 行
+    private int mergeRow;
+
+    // 合并单元格 列
+    private int mergeRank;
+
+    // 当前节点或者子节点在合并的情况下 真是宽度
+    private int realHigh;
+
+    // 当前节点或者子节点在合并的情况下 真是宽度
+    private int realWidth;
+
+    public Field mergeRow(int mergeRow) {
+        this.mergeRow = mergeRow;
+
+
+        return this;
+    }
+
+    public Field mergeRank(int mergeRank) {
+        this.mergeRank = mergeRank;
+
+
+        return this;
+    }
+
+    protected Integer getRealHigh() {
+        return realHigh;
+    }
+
+    protected Integer getRealWidth() {
+        return realWidth;
+    }
+
+
+
+    protected int getBreath() {
+        return breath;
+    }
+
+    protected int getDepth() {
+        return depth;
+    }
+
+    protected boolean isLeaf() {
+        return isLeaf;
+    }
+
+
+    /**
+     *
+     * 工具方法区域
+     */
+
+    private int reRealWidth(Field[] childFiles) {
+        int re = 0;
+        for (Field field : childFiles) {
+            re = re + field.getRealWidth();
+        }
+        return re;
+    }
+
+    private int reRealHigh(Field[] childFiles) {
+        int re = 0;
+        for (Field field : childFiles) {
+            re = re > field.getRealHigh() ? re : field.getRealHigh();
+        }
+        return re + 1;
+    }
+
     private int reBreath(Field[] childFiles) {
         int re = 0;
         for (Field field : childFiles) {
@@ -63,36 +175,43 @@ public class Field {
         return re + 1;
     }
 
-
-    public Field(String fieldName, String likeName, Field[] childFiles) {
-        this.fieldName = fieldName;
-        this.likeName = likeName;
-        this.childFiles = childFiles;
-        this.breath = reBreath(childFiles);
-        this.depth = reDepth(childFiles);
+    private int[] reBreathAndRealWidth(Field[] childFiles) {
+        int reBreath = 0;
+        int reRealWidth = 0;
+        for (Field field : childFiles) {
+            reBreath = reBreath + field.getBreath();
+            reRealWidth = reRealWidth + field.getRealWidth();
+        }
+        return new int[]{reBreath, reRealWidth};
     }
 
-    public Field(String fieldName, Field[] childFiles) {
-        this.fieldName = fieldName;
-        this.likeName = fieldName;
-        this.childFiles = childFiles;
-        this.breath = reBreath(childFiles);
-        this.depth = reDepth(childFiles);
+    private int[] reDepthAndRealHigh(Field[] childFiles) {
+        int reDepth = 0;
+        int reRealHigh = 0;
+        for (Field field : childFiles) {
+            reDepth = reDepth > field.getDepth() ? reDepth : field.getDepth();
+            reRealHigh = reRealHigh > field.getRealHigh() ? reRealHigh : field.getRealHigh();
+        }
+        reDepth = reDepth + 1;
+        reRealHigh = reRealHigh + 1 + mergeRow;
+        return new int[]{reDepth, reRealHigh};
     }
 
-    // 运行时 相关属性
-    // 节点的广度 : 有多少个子节点 逐步返回上级
-    private int breath;
-    // 节点的深度 : 有多少层子节点 逐步返回上级
-    private int depth;
-
-    public int getBreath() {
-        return breath;
+    @Override
+    public String toString() {
+        return "Field{" +
+                "fieldName='" + fieldName + '\'' +
+                ", likeName='" + likeName + '\'' +
+                ", isProtect=" + isProtect +
+                ", formula='" + formula + '\'' +
+                ", isLeaf=" + isLeaf +
+                ", childFiles=" + Arrays.toString(childFiles) +
+                ", breath=" + breath +
+                ", depth=" + depth +
+                ", mergeRow=" + mergeRow +
+                ", mergeRank=" + mergeRank +
+                ", realHigh=" + realHigh +
+                ", realWidth=" + realWidth +
+                '}';
     }
-
-    public int getDepth() {
-        return depth;
-    }
-
-
 }
--- src/main/java/io/sssd/model/TempletPart.java
+++ src/main/java/io/sssd/model/TempletPart.java
@@ -1,7 +1,8 @@
 package io.sssd.model;
 
-
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * Created by MIAOM on 2018/4/22.
@@ -15,27 +16,170 @@ public class TempletPart {
 
     private Collection data;
 
+    private String footer;
+
     private boolean showTitle = true;
 
+    public TempletPart header(String header) {
+        this.header = header;
+        return this;
+    }
 
+
+    private Integer breath;
+
+    private Integer depth;
+
+    private HashMap fieldMap;
+
+    public TempletPart fields(Field[] fields) {
+        this.fields = fields;
+        int breath = 0;
+        int depth = 0;
+        for (Field field : fields) {
+            breath = field.getBreath() + breath;
+            depth = depth > field.getDepth() ? depth : field.getDepth();
+        }
+        this.breath = breath;
+        this.depth = depth;
+        this.fieldMap = reFieldMap(fields);
+        return this;
+    }
+
+    public TempletPart data(Collection data) {
+        this.data = data;
+        this.dataCount = data.size();
+        return this;
+    }
+
+    public TempletPart footer(String footer) {
+        this.footer = footer;
+        return this;
+    }
+
+    public TempletPart hideTitle() {
+        this.showTitle = false;
+        return this;
+    }
 
     // key
     private String key;
 
     private Class entityClass;
 
+    private TempletPart() {
+    }
+
     public TempletPart(String key, Class entityClass) {
         this.key = key;
         this.entityClass = entityClass;
     }
 
-    private Integer titleRowNumber;
+    private Integer realRowNum;
 
-    private Integer startRowNumber;
+    private Integer headerRowNum;
 
-    private Integer endRowNumber;
+    private Integer opTitleRowNum;
 
+    private Integer edTitleRowNum;
+
+    private Integer opDataRowNum;
+
+    private Integer edDataRowNum;
 
     private int dataCount;
 
+
+    /**
+     * get
+     */
+
+    public String getHeader() {
+        return header;
+    }
+
+    public Field[] getFields() {
+        return fields;
+    }
+
+    public Collection getData() {
+        return data;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public boolean isShowTitle() {
+        return showTitle;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Class getEntityClass() {
+        return entityClass;
+    }
+
+    public Integer getRealRowNum() {
+        return realRowNum;
+    }
+
+    public Integer getHeaderRowNum() {
+        return headerRowNum;
+    }
+
+    public Integer getOpTitleRowNum() {
+        return opTitleRowNum;
+    }
+
+    public Integer getEdTitleRowNum() {
+        return edTitleRowNum;
+    }
+
+    public Integer getOpDataRowNum() {
+        return opDataRowNum;
+    }
+
+    public Integer getEdDataRowNum() {
+        return edDataRowNum;
+    }
+
+    public int getDataCount() {
+        return dataCount;
+    }
+
+    private HashMap<String, String> reFieldMap(Field[] fields) {
+        LinkedHashMap hashMap = new LinkedHashMap();
+        return reFieldMap(fields, hashMap);
+    }
+
+    private HashMap<String, String> reFieldMap(Field[] fields, HashMap<String, String> hashMap) {
+        for (Field field : fields) {
+            if (field.isLeaf()) {
+                hashMap.put(field.getFieldName(), field.getLikeName());
+            } else {
+                HashMap<String, String> hashMapc = this.reFieldMap(field.getChildFiles(), hashMap);
+                hashMap.putAll(hashMapc);
+            }
+        }
+        return hashMap;
+    }
+
+    private HashMap<String, Integer[]> reMergeMap(Field[] fields, HashMap<String, Integer[]> hashMap, int initRow, int initRank) {
+        for (Field field : fields) {
+            if (field.isLeaf()) {
+                int mergeRow = this.depth;
+                int mergeRank = field.getBreath();
+                Integer[] is = new Integer[]{initRow, initRank, mergeRow, mergeRank};
+                hashMap.put(field.getFieldName(), is);
+                initRank = mergeRank;
+            }else {
+
+
+            }
+        }
+        return hashMap;
+    }
 }
